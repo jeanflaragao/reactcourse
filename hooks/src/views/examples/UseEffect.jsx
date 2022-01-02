ca873c612909417f441cc4fad515b615002b86b4
Jean <aragao.jean@gmail.com>
2022-01-02 15:47:36 -0300

    Usando o hook effect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,15 +1,70 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcFatorial(num) {
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n == 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
-export default UseEffect
+function parImpar(num){
+    if (num % 2 === 0) return "Par"
+    return "Ímpar"
+} 
+
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+  const [status, setStatus] = useState("Ímpar");
+
+  useEffect(
+    function () {
+      setFatorial(calcFatorial(number))
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      setStatus((number % 2 === 0) ? "Par" : "Ímpar")
+    },
+    [number]
+  );
+
+
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <div>
+          <span className="text">Fatorial: </span>
+          <span className="text red">{fatorial}</span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <div>
+          <span className="text">Status: </span>
+          <span className="text red">{status}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
